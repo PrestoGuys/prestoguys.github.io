0386d4956e6a0b483539712da34837b54edf89cc
PrestoGuys <prestoguys@gmail.com>
2025-02-13 08:32:59 -0600

    fixed bugs and weird things improved everything

--- random-links/js/main.js
+++ random-links/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const jsonVersion = '4'; // Increment this version number each time you update the JSON file
-    fetch(`RandomLinks.json?v=${jsonVersion}`)
+    const jsonVersion = '5'; // Increment this version number each time you update the JSON file
+    fetch(`meta.json?v=${jsonVersion}`)
         .then(response => response.json())
         .then(data => {
             displayData(data);
@@ -13,17 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const itemsList = document.getElementById('items');
         itemsList.innerHTML = ''; // Clear existing items
-
-        data.items.forEach(item => {
-            const listItem = document.createElement('div');
-            listItem.className = 'gg';
-            listItem.innerHTML = `
-                <a href="${item.link}" target="_blank"><h3>${item.name}</h3></a>
-                <p><b>${item.description}</b></p>
-                <p>${item.mydescription}</p>
-                <br>
-            `;
-            itemsList.appendChild(listItem);
-        });
     }
 });
